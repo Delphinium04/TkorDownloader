983f8daaebd04d49aa975a3145c78328bc91cad6
이유찬 <delphinium04@outlook.com>
2023-03-15 14:05:06 +0900

    Edit
    Backup set

--- main.py
+++ main.py
@@ -6,31 +6,27 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 def set_chrome_driver():
+    # OLD
+    # driver = webdriver.Chrome(executable_path="./chromedriver", options=chrome_options)
+
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('headless')
-    driver = webdriver.Chrome(service=Service(
-        ChromeDriverManager().install()), options=chrome_options)
-    print('Selenium Started Successfully')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    print('Selenium Started')
     return driver
 
 
-def set_target_info():
-    ''' 상세 URL, Base URL, (웹툰 제목 순)'''
-    try:
-        print("웹툰 상세 페이지 입력")
-        url = input()
-        parsed = urlparse(url)
-        base = parsed.scheme + "://" + parsed.netloc
-        return url, base
-    except:
-        print("input_url parsing error, Exit")
-        exit()
+def check_url_validity(url):
+    parsed = urlparse(url)
+    base = parsed.scheme + "://" + parsed.netloc
+    if parsed.scheme == '' or parsed.netloc == '':
+        return ""
+    return base
 
 
 # Requests + BS4
@@ -40,52 +36,62 @@ def request_page(url):
         soup = BeautifulSoup(html.text, 'html.parser')
         return soup
     else:
-        print("Request Failed, Status: %d" % (html.status_code))
+        print("Request Failed, Status: %d" % html.status_code)
         return None
 
 
 def get_webtoon_name(page):
-    try:
-        info_table = page.select_one('table.bt_view1')
-        title_tag = info_table.select_one('td.bt_title')
-        name = title_tag.string.strip()
-        if name != "":
-            return title_tag.string.strip()
-    except:
-        return "WebtoonName_Error"
+    info_table = page.select_one('table.bt_view1')
+    title_tag = info_table.select_one('td.bt_title')
+    name = title_tag.string.strip()
+    if name != "":
+        return title_tag.string.strip()
+    else:
+        print("Couldn't get webtoon name")
+        exit()
 
 
 def get_ch_urls(page):
     ch_table = page.select_one('table.web_list')
     title_tag = ch_table.select('td.content__title')
+
     ch_urls = []
     for tag in title_tag:
         ch_urls.append(base_url + tag.get('data-role'))
-        # Latest Chapter -> First Chapter
+        # Download recent Chapter to old chapter, descending
+    ch_urls.reverse()
     return ch_urls
 
 
 # Selenium
 def get_webtoon_img_urls(_url):
-    try:
-        driver.get(_url)
-        div_toon_img = driver.find_element(by=By.ID, value="toon_img")
-        img_tags = div_toon_img.find_elements(by=By.TAG_NAME, value='img')
+    driver.get(_url)
+    div_toon_img = driver.find_element(by=By.ID, value="toon_img")
+    img_tags = div_toon_img.find_elements(by=By.TAG_NAME, value='img')
 
+    if img_tags:
         images_url = []
         for img in img_tags:
             images_url.append(img.get_attribute('src'))
-    except:
-        print("webtoon url crawling error")
+        return images_url
+    else:
+        print("Couldn't get images url")
         return []
-    return images_url
 
 
 # By Chapter
-def download_images(_urls):
+def download_images(_urls, count):
     title = driver.title
-    folder_path = f'./{webtoon_name}/{driver.title}'.translate(
-        str.maketrans('', '', "\\:?\"<>|"))
+    folder_path = './{0}/{1:03d}-{2}'.format(webtoon_name, count, title)
+    folder_path = folder_path.translate(str.maketrans('', '', "\\:?\"<>|"))
+    if os.path.isdir(folder_path):
+        print('폴더 존재 존재');
+        exist_file_list = os.listdir(folder_path)
+        if len(exist_file_list) == len(_urls):
+            print("파일 존재, 다음 화 스킵")
+            return
+        else:
+            print("강제 다운로드")
     os.makedirs(folder_path, exist_ok=True)
 
     count = 1
@@ -104,41 +110,47 @@ def download_images(_urls):
         count += 1
         with open(file_path, 'wb') as f:
             f.write(img.content)
-        print(f'{count-1}/{len(urls)} => {time.time() - start} seconds')
+        print(f'{count - 1}/{len(urls)} => {time.time() - start} seconds')
 
     print('\n')
     return
 
 
-driver = set_chrome_driver()
-
-while (True):
-    input_url, base_url = set_target_info()
+if __name__ == '__main__':
     driver = set_chrome_driver()
-    bs4_soup = request_page(input_url)
-    if bs4_soup is None:
-        print("Requested Html Error (NONE)")
-        exit()
 
-    webtoon_name = get_webtoon_name(bs4_soup).translate(
-        str.maketrans('', '', "\\:?\"<>|"))
-    chapter_urls = get_ch_urls(bs4_soup)
+    while True:
+        print("주소를 입력 (예시: https://toonkorXXX.com/???)")
+        user_input = input()
+        base_url = check_url_validity(user_input)
 
-    print("%s Downloading Start\n" % (webtoon_name))
-    for chapter_url in chapter_urls:
-        print(f"Get Chapter's Urls...\n")
-        image_urls = get_webtoon_img_urls(chapter_url)
+        if base_url == "":
+            print("URL 오류, 다시 입력")
+            continue
 
-        print("Ok. Ready Image...\n")
-        download_images(image_urls)
+        driver = set_chrome_driver()
+        bs4_soup = request_page(user_input)
+        if bs4_soup is None:
+            print("bs4 request_page error")
+            continue
 
-        print(f'{chapter_url} Passed\n\n')
+        webtoon_name = get_webtoon_name(bs4_soup)
+        chapter_urls = get_ch_urls(bs4_soup)
+        print("%s Downloading Start\n" % webtoon_name)
 
-    print("Again? Y / N")
-    answer = input()
-    if answer == "Y" or answer == 'y':
-        continue
-    else:
-        break
+        chapter_count = 1
+        for chapter_url in chapter_urls:
+            print(f"Get Chapter's Urls")
+            image_urls = get_webtoon_img_urls(chapter_url)
+            print("Downloading Image")
+            download_images(image_urls, chapter_count)
+            print(f'\n{chapter_url} Passed\n')
+            chapter_count = chapter_count + 1
 
-exit()
+        print("Again? Y / N")
+        answer = input()
+        if answer == "Y" or answer == 'y':
+            continue
+        else:
+            break
+
--- mkdirtest.py
+++
@@ -1,8 +0,0 @@
-import os
-import string
-
-path = './dir/히어로의 남친인데요? 3화/'
-
-print(path)
-
-os.makedirs(path, exist_ok=True)
